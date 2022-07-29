4eb5aa09d70543ca3de4bff0a8d89068d35931d9
Samuel Balco <goodlyrottenapple@gmail.com>
2022-07-29 10:52:11 +0100

    Added `check-submodules` and `update-from-submodules` functions (#3)

---
+++ flake.nix
@@ -0,0 +1,53 @@
+{
+  description = "Pure Nix flake utility functions used in other RV repos";
+  outputs = { self }: {
+    lib.check-submodules = pkgs: dependencies:
+      let
+        hashes = with builtins;
+          map (key: {
+            name = key;
+            rev = dependencies.${key}.rev;
+          }) (attrNames dependencies);
+      in pkgs.writeShellScriptBin "check-versions" ''
+        STATUS=$(git submodule status --recursive);
+        for elem in ${
+          pkgs.lib.concatMapStringsSep " " ({ name, rev }: "${name},${rev}")
+          hashes
+        }; do
+          IFS=","; set -- $elem;
+          if ! grep -q "$2" <<< "$STATUS";
+          then
+              echo "$1 with hash '$2' does not match any current submodules:"
+              echo "$STATUS"
+              exit 1
+          fi
+        done
+        echo "All dependencies match"
+      '';
+    lib.update-from-submodules = pkgs: lockFile: dependencies:
+      let
+        lock = (builtins.fromJSON (builtins.readFile lockFile)).nodes;
+        deps = with builtins;
+          map (key: {
+            name = key;
+            var = builtins.replaceStrings [ "-" "_" ] [ "" "" ]
+              (pkgs.lib.toUpper key);
+            submodule = dependencies.${key}.submodule;
+            owner = lock.${key}.original.owner;
+            repo = lock.${key}.original.repo;
+          }) (attrNames dependencies);
+      in pkgs.writeShellScriptBin "check-versions" ''
+        ${pkgs.lib.concatMapStringsSep "\n"
+        ({ var, submodule, ... }:
+          "${var}=$(git rev-parse HEAD:${submodule})") deps}
+
+        nix flake lock \
+        ${
+          pkgs.lib.concatMapStringsSep " \\\n"
+          ({ name, var, owner, repo, ... }:
+            "  --override-input ${name} github:${owner}/${repo}/\$${var}") deps
+        } \
+          --recreate-lock-file 
+      '';
+  };
+}
